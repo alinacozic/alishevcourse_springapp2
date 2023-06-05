705ec9b680847fd85fea9494e63d17aceb6eadb5
ealicoz <alina.cozic@gmail.com>
2023-06-05 13:24:29 +0300

    springapp2

---
+++ src/main/Java/com/alina/springcourse/ClassicalMusic.java
@@ -0,0 +1,9 @@
+package com.alina.springcourse;
+
+public class ClassicalMusic implements Music{
+
+    @Override
+    public String getSong(){
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/Java/com/alina/springcourse/Music.java
@@ -0,0 +1,5 @@
+package com.alina.springcourse;
+
+public interface Music {
+    String getSong();
+}
